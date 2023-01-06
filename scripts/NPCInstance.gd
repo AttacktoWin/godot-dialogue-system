63db38b530f5834eff904062d4e03b3559d2c002
Shashank Bhat <shawshankbhat@gmail.com>
2023-01-05 20:39:48 -0700

    Added basic logic for interacting with an NPCInstance

--- scripts/NPCInstance.gd
+++ scripts/NPCInstance.gd
@@ -1,9 +1,16 @@
 @tool
 extends Node2D
 
-const dialogue_system = preload("res://scripts/DialogueSystem.gd");
+const DialogueSystem = preload("res://scripts/DialogueSystem.gd");
 
 @export var NPC_id: String = "";
 
+var interacted := false;
+
 func interact() -> void:
-	pass
+	var system = get_node("/root/DialogueSystem") as DialogueSystem;
+	var dialogue = system.get_top_dialogue(NPC_id);
+	if (dialogue != null):
+		# TODO: display the dialogue in a text box
+		system.dialogue_viewed(NPC_id, dialogue.id);
+		interacted = true;
